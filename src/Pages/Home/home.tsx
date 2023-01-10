3ce018deaf78482a90cb249a580ec66a18201e85
Arslan Rasheed <arslanrashee1@gmail.com>
2023-01-10 19:41:33 +0500

    initializing menu bar

---
+++ src/Pages/Home/home.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import { Box } from '@chakra-ui/react';
+
+const HomePage = () => {
+    return ( <Box></Box> );
+}
+ 
+export default HomePage;
---
+++ src/Pages/Menu/SideMenu.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+import { Box } from '@chakra-ui/react';
+import { createUseStyles } from 'react-jss';
+
+
+const useStyles = createUseStyles({
+    menuImage: {
+            width: '120px',
+
+
+//         width: '120px',
+//   height: 120px;
+//   overflow: hidden;
+//   display: inline-block;
+//   vertical-align: top;
+//   border-radius: 50%;
+//   border: 5px solid #fff;
+    }
+})
+
+
+const SideMenuBar = () => {
+    return ( <Box width={'250px'} position={'fixed'} bottom={0} top={0} backgroundColor={'#0c0c14'}>
+    <Box height={'calc(100% - 50px);'}>
+        <Box textAlign={'center'} position={'relative'} padding={'65px 25px 25px'}>
+            <Box textColor={"#ffffff"}>
+                logo
+            </Box>
+        </Box>
+    </Box>
+</Box> );
+}
+ 
+export default SideMenuBar;
