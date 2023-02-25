68b51d94db51ef66e8e2b25839566697d271e2ad
Mehran Rasheed <engr.mehranrasheed@gmail.com>
2023-02-25 13:07:25 +0100

    Initialized home page.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
+import Home from "../components/home/Home";
 
 const HomePage = () => {
-  return <Box zIndex={"-2"}>Home</Box>;
+  return (
+    <Box zIndex={"-2"}>
+      <Home />
+    </Box>
+  );
 };
 
 export default HomePage;
