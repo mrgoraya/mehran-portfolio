1c2403c8e8f563d6c69350e10edd7d5d8b708d4c
Mehran Rasheed <engr.mehranrasheed@gmail.com>
2023-02-28 22:56:15 +0100

    Changed them folder.

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
-import customTheme from "./themes/theme";
+import customTheme from "./styles/theme";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
